2c839158897a6ae5b59642038b4199527a3691a1
Jamie <jamie.yjchen@gmail.com>
2021-03-25 00:35:20 -0700

    add default timeline from documentation
    https://material-ui.com/components/timeline/

--- src/components/Timeline/ResumeTimeline.js
+++ src/components/Timeline/ResumeTimeline.js
@@ -6,14 +6,38 @@ import TimelineSeperator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
+// material ui
+import { Typography } from '@material-ui/core';
 // css
 import '../../css/timeline.css';
 
 const ResumeTimeline = () => {
     return (
-        <div>
-            I am the timeline.
-        </div>
+        <Timeline className={"timeline"}>
+            {/* first item */}
+            <TimelineItem className={"timeline_container first-item"}>
+                <TimelineSeperator>
+                    <TimelineDot className={"timeline_header"}/>
+                    <TimelineConnector />
+                </TimelineSeperator>
+                <TimelineContent><Typography variant="h6" className="">Apple</Typography></TimelineContent>
+            </TimelineItem>
+            {/* items in the middle */}
+            <TimelineItem className={"timeline_container"}>
+                <TimelineSeperator>
+                    <TimelineDot className={"timeline_dot"}/>
+                    <TimelineConnector />
+                </TimelineSeperator>
+                <TimelineContent>Banana</TimelineContent>
+            </TimelineItem>
+            {/* last item */}
+            <TimelineItem className={"timeline_container last-item"}>
+                <TimelineSeperator>
+                    <TimelineDot className={"timeline_dot"}/>
+                </TimelineSeperator>
+                <TimelineContent>Cherry</TimelineContent>
+            </TimelineItem>
+        </Timeline>
     )
 }
 
